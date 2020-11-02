6963784e26ecb248ffb5ed2db530ad3d63a682a6
akila liyanage <mlakilaliyanage@gmail.com>
2020-11-02 08:41:39 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Creative-CMB/creativecmb/precache-manifest.f9360e6344463c4e9dcb8ff96da7811a.js"
+  "/creativecmb/precache-manifest.c060e7d33d1e703c15eb029162013caa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Creative-CMB/creativecmb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/creativecmb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
