fb0550d1e5e80007d7ab8cd18022ca2db332d705
akila liyanage <mlakilaliyanage@gmail.com>
2020-12-17 12:09:53 +0530

    Updates

---
+++ precache-manifest.1a59886aaabe4ba57ce0f10111cc2cfc.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a6f434ba369718233abfb74cff322d",
+    "url": "/creativecmb/index.html"
+  },
+  {
+    "revision": "546ec1058f3356e0ca80",
+    "url": "/creativecmb/static/css/2.617c1865.chunk.css"
+  },
+  {
+    "revision": "6f9ae67fe381ada22548",
+    "url": "/creativecmb/static/css/main.34281a2a.chunk.css"
+  },
+  {
+    "revision": "546ec1058f3356e0ca80",
+    "url": "/creativecmb/static/js/2.cb888a81.chunk.js"
+  },
+  {
+    "revision": "cb19517c8abb6f7aea1fef2173cfeb8d",
+    "url": "/creativecmb/static/js/2.cb888a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f9ae67fe381ada22548",
+    "url": "/creativecmb/static/js/main.aa00e45e.chunk.js"
+  },
+  {
+    "revision": "756c02ba8c34ffe0df5a",
+    "url": "/creativecmb/static/js/runtime-main.42e6fc18.js"
+  },
+  {
+    "revision": "04353f25530b26485e3747e09970f122",
+    "url": "/creativecmb/static/media/IMG_2619.04353f25.png"
+  },
+  {
+    "revision": "a46d42c753d5ab76d596c3da11c7f046",
+    "url": "/creativecmb/static/media/IMG_2627.a46d42c7.png"
+  },
+  {
+    "revision": "5c2ea4cc3a858ac0f7108b2eebed12bd",
+    "url": "/creativecmb/static/media/IMG_6662.5c2ea4cc.HEIC"
+  },
+  {
+    "revision": "7e4ad96eb995e3ad93a344d2c11a07da",
+    "url": "/creativecmb/static/media/LED.7e4ad96e.JPG"
+  },
+  {
+    "revision": "d36f2c6baa9109ef7f5eba86da79ab04",
+    "url": "/creativecmb/static/media/ad.d36f2c6b.png"
+  },
+  {
+    "revision": "dcb4c8b406358b61157fd7c79e06190b",
+    "url": "/creativecmb/static/media/ad1.dcb4c8b4.jpg"
+  },
+  {
+    "revision": "3d45f423d62a5020c05c928ad8772391",
+    "url": "/creativecmb/static/media/arrow.3d45f423.svg"
+  },
+  {
+    "revision": "a74212b632dc205e2bd832acaa46a2e9",
+    "url": "/creativecmb/static/media/arrow.a74212b6.svg"
+  },
+  {
+    "revision": "f4c9e632003bdb83c7685632e456f360",
+    "url": "/creativecmb/static/media/contact.f4c9e632.png"
+  },
+  {
+    "revision": "3dd216c55f39f3de04b79389562bd0a3",
+    "url": "/creativecmb/static/media/dhllogo.3dd216c5.png"
+  },
+  {
+    "revision": "db57da795c9c521b14cf9f487da06d05",
+    "url": "/creativecmb/static/media/dribble.db57da79.svg"
+  },
+  {
+    "revision": "30aa833ba88cb552cce0bf332708f5b7",
+    "url": "/creativecmb/static/media/event.30aa833b.png"
+  },
+  {
+    "revision": "aa1e8c9c8592f04d2d3d68c86a746696",
+    "url": "/creativecmb/static/media/facebook.aa1e8c9c.svg"
+  },
+  {
+    "revision": "ab8946ca4fdadfd36f217999a683ac0a",
+    "url": "/creativecmb/static/media/glaxo.ab8946ca.png"
+  },
+  {
+    "revision": "fb8accae328d10b9077fbf81fd27081c",
+    "url": "/creativecmb/static/media/hero4.fb8accae.png"
+  },
+  {
+    "revision": "1d202029fc44a3ef5e9233b2633b3268",
+    "url": "/creativecmb/static/media/hero5.1d202029.png"
+  },
+  {
+    "revision": "e61fa1c1440f22a9b6ceef0a481a81c4",
+    "url": "/creativecmb/static/media/hero6.e61fa1c1.png"
+  },
+  {
+    "revision": "8998c74b46bc06d70230d41135fc65f0",
+    "url": "/creativecmb/static/media/hero7.8998c74b.png"
+  },
+  {
+    "revision": "b3993a9c5d37eff88b7508cc97d09de7",
+    "url": "/creativecmb/static/media/hika.b3993a9c.png"
+  },
+  {
+    "revision": "cf265fbea1ac725c7cf6a8923d8164fd",
+    "url": "/creativecmb/static/media/hsbc.cf265fbe.png"
+  },
+  {
+    "revision": "8acad035ca4e89d4d50aaba2a83b0b63",
+    "url": "/creativecmb/static/media/microsoftlogo.8acad035.png"
+  },
+  {
+    "revision": "f521cf4553d6d240c47e135be9ba7a9c",
+    "url": "/creativecmb/static/media/mobile-close.f521cf45.svg"
+  },
+  {
+    "revision": "5a480759eea0f1f74fddf2f5c8011161",
+    "url": "/creativecmb/static/media/naweed.5a480759.png"
+  },
+  {
+    "revision": "3aae30e44f4f261bc4748f2e782ae798",
+    "url": "/creativecmb/static/media/pepsilogo.3aae30e4.png"
+  },
+  {
+    "revision": "50fda3b1451d88852dca1dc13330ccc5",
+    "url": "/creativecmb/static/media/port10.50fda3b1.png"
+  },
+  {
+    "revision": "5613d103db9caac1de3443e78e29e7ed",
+    "url": "/creativecmb/static/media/port2.5613d103.png"
+  },
+  {
+    "revision": "bc9eb539a991e19abe0051361f0cd1ef",
+    "url": "/creativecmb/static/media/port3.bc9eb539.png"
+  },
+  {
+    "revision": "789895b7e330a6ecba43347813615ca8",
+    "url": "/creativecmb/static/media/port4.789895b7.png"
+  },
+  {
+    "revision": "4a92e24e798fe85cf9a2b2932efa889f",
+    "url": "/creativecmb/static/media/port5.4a92e24e.png"
+  },
+  {
+    "revision": "63e5b418cd93ef7382055a3e45a9b784",
+    "url": "/creativecmb/static/media/port8.63e5b418.png"
+  },
+  {
+    "revision": "290f12d6a98b61a30c8189ceb2514edb",
+    "url": "/creativecmb/static/media/rent1.290f12d6.png"
+  },
+  {
+    "revision": "9d4487367c19a0f7e2063f8112015d53",
+    "url": "/creativecmb/static/media/ships.9d448736.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/creativecmb/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/creativecmb/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/creativecmb/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/creativecmb/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "11227e1c608dbb2eb8a2fdba5fef0d2d",
+    "url": "/creativecmb/static/media/team.11227e1c.JPG"
+  },
+  {
+    "revision": "bb7b5d1c604c0fec5584555ecb8d8549",
+    "url": "/creativecmb/static/media/trigent.bb7b5d1c.png"
+  },
+  {
+    "revision": "0ecdf21ae145f462c5258daad33c2dda",
+    "url": "/creativecmb/static/media/twitter.0ecdf21a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b78674e7232df68765a90320ac216ddb.js"
+  "/creativecmb/precache-manifest.1a59886aaabe4ba57ce0f10111cc2cfc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/creativecmb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
